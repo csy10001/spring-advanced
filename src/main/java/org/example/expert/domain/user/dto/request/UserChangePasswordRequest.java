8b6897231bc6d70370499675fcdd65746f079c23
csy10001 <fuitshine@gmail.com>
2025-08-29 09:58:51 +0900

    fix : 레벨 2-3 Validation dto로 고쳤습니다.

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
+    // 새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.
     @NotBlank
     private String oldPassword;
     @NotBlank
+    @Size(message = "새 비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*[0-9].*", message = "비밀번호에는 숫자가 있어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "비밀번호에는 대문자가 있어야 합니다.")
     private String newPassword;
 }

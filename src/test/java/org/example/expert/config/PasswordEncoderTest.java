4452a7272b32ddd7537a2262635d553094e20354
csy10001 <fuitshine@gmail.com>
2025-08-29 15:51:34 +0900

    fix : 테스트 코드 연습 - 1

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -16,11 +16,15 @@ class PasswordEncoderTest {
     @Test
     void matches_메서드가_정상적으로_동작한다() {
         // given
+
+        // encodedPassword는 rawPassword를 암호화 한다
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
         // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // match의 내부 로직은 첫번째 변수를 받아서 암호화를 하고 두번째 인자(위에서 미리 암호화를 했다)와 서로 같은지 확인
+        // 그래서 match는 순서가 중요하다 !
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
         // then
         assertTrue(matches);
